9ecb462ecbbe58f8b293b2989869382867a1d0d8
Leelakalyan <99253536+Leelakalyan@users.noreply.github.com>
2022-02-18 21:27:44 +0530

    Create Sum of factorial.py

---
+++ Sum of factorial.py
@@ -0,0 +1,15 @@
+f=1
+sum=0
+n=int(input('enter n value:'))
+for i in range(1,n+1):
+    f=f*i
+    sum=sum+f
+
+print('factorial of given no is:',f)
+print('sum of factorial is:',sum)
+
+Output:
+enter n value:
+6
+factorial of given no is: 720
+sum of factorial is: 873
